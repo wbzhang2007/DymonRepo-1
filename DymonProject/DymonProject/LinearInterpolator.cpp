5e474c6a1c24362061b2dbad8d28df6b4ff63c6d
Neoracle <theone_329@hotmail.com>
2012-12-11 23:55:30 +0800

    update linearInterpolator

--- DymonProject/DymonProject/LinearInterpolator.cpp
+++ DymonProject/DymonProject/LinearInterpolator.cpp
@@ -10,7 +10,9 @@ LinearInterpolator::LinearInterpolator(point startPoint, point endPoint):
 	AbstractInterpolator(startPoint, endPoint){}
 
 point LinearInterpolator::interpolateS(date date0){
-	double slope = (std::get<1>(_endPoint) - std::get<1>(_startPoint))/(std::get<0>(_endPoint).getJudianDayNumber() - std::get<0>(_startPoint).getJudianDayNumber());
+	double yDiff = std::get<1>(_endPoint) - std::get<1>(_startPoint);
+	double xDiff = std::get<0>(_endPoint).getJudianDayNumber() - std::get<0>(_startPoint).getJudianDayNumber();
+	double slope = yDiff / xDiff;
 	double yVal = slope*date0.getJudianDayNumber()+std::get<1>(_startPoint);
 	return point(date0, yVal);
 }
